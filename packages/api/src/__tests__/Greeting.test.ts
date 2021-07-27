9304e279cd8b4f4164426744087cd6b023045068
Dan Kelosky <dkelosky@gmail.com>
2021-07-27 11:39:02 -0400

    debugging
    Signed-off-by: Dan Kelosky <dkelosky@gmail.com>

--- packages/api/src/__tests__/Greeting.test.ts
+++ packages/api/src/__tests__/Greeting.test.ts
@@ -1,13 +1,17 @@
-import { Greeting } from '../Greeting';
+/**
+ * This program and the accompanying materials are made available and may be used, at your option, under either:
+ * * Eclipse Public License v2.0, available at https://www.eclipse.org/legal/epl-v20.html, OR
+ * * Apache License, version 2.0, available at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+ *
+ * Copyright Contributors to the Zowe Project.
+ */
+
 
 describe("Greeting tests", () => {
     it("should return 'Hello'", () => {
 
-
-        let greeting = new Greeting();
-
-        const test = "hey";
-
-        expect(test).toEqual("hey");
+        expect(true).toBe(true);
     });
 });
