b1a78fa60dc6bf93fb27d79aa0405be30e0d36f9
Dan Kelosky <dkelosky@gmail.com>
2021-07-30 10:56:34 -0400

    break a test
    Signed-off-by: Dan Kelosky <dkelosky@gmail.com>

--- packages/cli/src/__tests__/SampleSessionUtils.test.ts
+++ packages/cli/src/__tests__/SampleSessionUtils.test.ts
@@ -10,6 +10,6 @@
 
 describe("SampleSessionUtils tests", () => {
   it("should test getting a session from our utils", () => {
-    expect(true).toBe(true);
+    expect(false).toBe(true);
   });
 });
